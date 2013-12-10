ee3516df420cc7ddafac4679ae4c39b8a0088a07
Depech Vogel <depechvogelnl@gmail.com>
2013-12-10 17:43:29 +0000

    2

--- play/recipes/oj.rb
+++ play/recipes/oj.rb
@@ -1,9 +1,5 @@
 if platform?('debian', 'ubuntu')
-  apt_repository 'oj' do
-    uri "http://ppa.launchpad.net/wepupd8team/java/ubuntu"
-    distribution node['lsb']['codename']
-    components ["main"]
-    keyserver "keyserver.ubuntu.com"
-    key "EEA14886"
+  execute "enable oj PPA" do
+    command "add-apt-repository http://ppa.launchpad.net/webupd8team/java/ubuntu && apt-get update"
   end
 end
