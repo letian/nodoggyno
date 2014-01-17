044847b44930248916e2a27c62a243a03b8546aa
Depech Vogel <depechvogelnl@gmail.com>
2014-01-17 08:19:38 +0000

    Add setavar 2

--- setavar/recipes/wabbit.rb
+++
@@ -1,8 +0,0 @@
-node[:deploy].each do |application, deploy|
- rabbitIP = node[:rabbitIP]
- file_dir = deploy[:deploy_to]
-
-  execute "Configure RabbitIP" do
-  command "echo \"<?php Configure::write('rabbitip', \'#{rabbitIP}\'); ?>\" > #{file_loc}/current/rabbitMQ.php"
-                               end
-end
